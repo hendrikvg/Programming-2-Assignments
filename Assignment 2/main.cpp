5da5486c1becc7482aaa350d74e247e98e15a65c
hendrikvg <63549321+hendrikvg@users.noreply.github.com>
2021-02-07 23:29:16 +0000

    Everything below line 75 is still pseudo code :^)

--- Assignment 2/main.cpp
+++ Assignment 2/main.cpp
@@ -7,51 +7,12 @@ using namespace std;
 
 const int boardSize = 12;
 
-// array<array<bool, boardSize>, boardSize> booleanBoard{
-//         1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ,1, 
-//         1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1,
-//         0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1,
-//         1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1,
-//         1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0,
-//         1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1,
-//         1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1,
-//         1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1,
-//         1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1,
-//         1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1,
-//         1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-//         1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+struct RowAndColumn {
+    int row;
+    int column;
+};
 
-// void showBoard()
-// {
-//     for (int row = 0; row < boardSize; row++)
-//     {
-//         for (int column = 0; column < boardSize; column++)
-//         {
-//             if (column == boardSize - 1)
-//             {
-//                 if (booleanBoard[row][column] == 1)
-//                 {
-//                     cout << " # " << endl;
-//                 } else {
-//                     cout << " . " << endl;
-//                 }
-
-//             } else
-//             {
-//                 if (board[row][column] == 1)
-//                 {
-//                     cout << " # ";
-//                 } else {
-//                     cout << " . ";
-//                 }
-                
-//             }
-            
-//         }
-//     }
-// }
-
-array<array<char, boardSize>, boardSize> characterBoard{
+array<array<char, boardSize>, boardSize> maze{
         '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' ,'#', 
         '#', '.', '.', '.', '#', '.', '.', '.', '.', '.', '.', '#',
         '.', '.', '#', '.', '#', '.', '#', '#', '#', '#', '.', '#',
@@ -63,7 +24,11 @@ array<array<char, boardSize>, boardSize> characterBoard{
         '#', '.', '.', '.', '.', '.', '.', '.', '.', '#', '.', '#',
         '#', '#', '#', '#', '#', '#', '.', '#', '#', '#', '.', '#',
         '#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#',
-        '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'};   
+        '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'};
+        
+array<array<bool, boardSize>, boardSize> visited;
+
+RowAndColumn mazeExit{2,0};
 
 void showBoard()
 {
@@ -73,18 +38,81 @@ void showBoard()
         {
             if (column == boardSize - 1)
             {
-                cout << " " << characterBoard[row][column] << endl;
+                cout << " " << maze[row][column] << endl;
             } else {
-                cout << " " << characterBoard[row][column] << " ";              
+                cout << " " << maze[row][column] << " ";              
             }
             
         }
     }
 }
 
+RowAndColumn linearSearch(char key)
+{
+    for (int row = 0; row < boardSize; row++)
+    {
+        for (int column = 0; column < boardSize; column++)
+        {
+            if (maze[row][column] == key)
+            {
+            RowAndColumn rowAndColumn;
+            rowAndColumn.row = row;
+            rowAndColumn.column = column; 
+            return rowAndColumn;
+            }
+        }
+    }
+    cout << "Key: \"" << key << "\" not found." << endl;
+} 
+
+bool traverseMaze(array<array<char, boardSize>, boardSize> walls, array<array<bool, boardSize>, boardSize> visited, RowAndColumn rowAndColumn) {
+  showBoard();
+  RowAndColumn location = linearSearch('x');
+  if (location.row == mazeExit.row && location.column == mazeExit.column) {
+    return true;
+  }
+
+  visited[location.row][location.column] = 1;
+  
+  if (walls(location + right) != wall && !visited(location + right)) {
+    if (path(walls, visited, location+right) {
+        return true;
+    } else {
+      showBoard();
+    }
+  }
+
+  if (walls(location + left) != wall && !visited(location + left)) {
+    if (path(walls, visited, location+left) {
+        return true;
+    } else {
+    showBoard();
+  }
+
+  if (walls(location + up) != wall && !visited(location + up)) {
+    if (path(walls, visited, location+up) {
+        return true;
+    } else {
+    showBoard();
+  }
+
+  if (walls(location + down) != wall && !visited(location + down)) {
+    if (path(walls, visited, location+down) {
+        return true;
+    } else {
+    showBoard();
+  }
+
+  // no direction was succesful, we hit a dead end thus we return false (and print the board)
+  return false;
+}
+
 int main()
 {
     cout << "Hello world!" << endl;
-    showBoard();    
+    showBoard();
+    RowAndColumn rowAndColumn = linearSearch('x');
+    cout << "Row:" << mazeExit.row << endl;
+    cout << "Column:" << mazeExit.column << endl;
     return 0;
 }
